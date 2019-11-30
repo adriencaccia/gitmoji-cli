df49cc73e484a5478b602358683561d4762e581e
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2019-11-30 23:05:14 +0100

    [2] ✨ Add ticket info on hook
    AA User, in config, I can list all my Trello boards and select one
    https://trello.com/c/DnL372eW

--- src/trello/api.js
+++ src/trello/api.js
@@ -1,5 +1,6 @@
 const axios = require('axios')
 const open = require('open')
+const fuzzy = require('fuzzy')
 const queryString = require('querystring')
 const constants = require('../constants')
 const config = require('../config')
@@ -61,6 +62,60 @@ class TrelloApi {
     } catch (error) {
       return 'Invalid token'
     }
+  }
+
+  boardsToSearchableSting (boards) {
+    return boards.map(board => {
+      const boardName = `📋: ${board.name.toLowerCase()}`
+      const organizationName = (board.organization && board.organization.displayName)
+        ? ` | 🏢: ${board.organization.displayName.toLowerCase()}` : ''
+      return {
+        name: boardName.concat(organizationName),
+        value: board.id
+      }
+    })
+  }
+
+  searchBoards (boards, input) {
+    input = input || ''
+    return new Promise((resolve) => {
+      const fuzzyResult = fuzzy.filter(
+        input,
+        this.boardsToSearchableSting(boards),
+        { extract: (el) => el.name }
+      )
+      resolve(
+        fuzzyResult.map(function (el) {
+          return el.original
+        })
+      )
+    })
+  }
+
+  trelloBoardsPrompt () {
+    const boards = config.getTrelloBoards()
+    return [
+      {
+        name: 'boardId',
+        message: 'Choose a Trello board:',
+        type: 'autocomplete',
+        source: (_, input) => this.searchBoards(boards, input)
+      }
+    ]
+  }
+
+  fetchTicketInfo (ticketNumber) {
+    const boardId = config.getTrelloBoardByPwd()
+    return this.trelloApiClient.request({
+      method: 'GET',
+      url: `/boards/${boardId}/cards/${ticketNumber}`,
+      params: {
+        fields: 'name,shortUrl,idShort',
+        checkItemStates: false,
+        token: config.getTrelloApiToken(),
+        key: constants.TRELLO_API_KEY
+      }
+    })
   }
 }
 
